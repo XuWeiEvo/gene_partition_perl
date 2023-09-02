036ac09054e80dd3fed9389ae12079ad751d39b4
Wei Xu <126555867+XuWeiEvo@users.noreply.github.com>
2023-09-02 11:49:00 +0800

    Add files via upload

---
+++ partion.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $input_file = 'input.csv';
+my $output_file = 'partition.txt';
+
+open(my $input_fh, '<', $input_file) or die "Cannot open input file: $!";
+open(my $output_fh, '>', $output_file) or die "Cannot open output file: $!";
+
+while (my $line = <$input_fh>) {
+    chomp $line;
+    my ($filename, $range) = split(',', $line);
+    my ($gene) = $filename =~ /^(.*?)_/;
+
+    my ($start, $end);
+    my ($codon_start, $codon_end, $length);
+
+    if ($gene =~ /^(12s|16s)$/) {
+        ($start, $end) = $range =~ /(\d+) => (\d+)/;
+        print $output_fh "DNA, $gene = $start-$end\n";
+    } else {
+        ($start, $end) = $range =~ /(\d+) => (\d+)/;
+        $length = $end - $start + 1;
+
+        for my $i (1..3) {
+            $codon_start = $start + ($i - 1);
+            $codon_end = $end;
+            print $output_fh "DNA, ${gene}_codon$i = $codon_start-$codon_end\\3\n";
+        }
+    }
+}
+
+close $input_fh;
+close $output_fh;
+
+print "Conversion completed. Results saved in partition.txt.\n";
