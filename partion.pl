224c11b98464c21f42278da3f4a8a2a7ef204f94
Wei Xu <126555867+XuWeiEvo@users.noreply.github.com>
2023-12-13 08:48:57 +0800

    Update partion.pl

--- partion.pl
+++ partion.pl
@@ -12,7 +12,7 @@ open(my $output_fh, '>', $output_file) or die "Cannot open output file: $!";
 while (my $line = <$input_fh>) {
     chomp $line;
     my ($filename, $range) = split(',', $line);
-    my ($gene) = $filename =~ /^(.*?)_/;
+    my ($gene) = $filename =~ /^(.*?)_/;  ##my ($gene) = $filename =~ /^([A-Za-z0-9_,]+)/;
 
     my ($start, $end);
     my ($codon_start, $codon_end, $length);
@@ -32,7 +32,46 @@ while (my $line = <$input_fh>) {
     }
 }
 
+
 close $input_fh;
 close $output_fh;
 
 print "Conversion completed. Results saved in partition.txt.\n";
+
+
+
+
+
+
+
+## If you do not want to split the three codon, then use the code below:
+
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $input_file = 'input.csv';
+my $output_file = 'partition.txt';
+
+open(my $input_fh, '<', $input_file) or die "Cannot open input file: $!";
+open(my $output_fh, '>', $output_file) or die "Cannot open output file: $!";
+
+while (my $line = <$input_fh>) {
+    chomp $line;
+    my ($filename, $range) = split(',', $line);
+    my ($gene) = $filename =~ /^([A-Za-z0-9_,]+)/;
+
+    my ($start, $end);
+
+
+        ($start, $end) = $range =~ /(\d+) => (\d+)/;
+        print $output_fh "DNA, $gene = $start-$end\n";
+     
+}
+
+close $input_fh;
+close $output_fh;
+
+print "Conversion completed. Results saved in partition.txt.\n";
+
